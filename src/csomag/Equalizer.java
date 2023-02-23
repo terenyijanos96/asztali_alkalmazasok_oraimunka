abf8cb14caf82a2da5f3ce60427e43e1e8cc1469
Terényi János <terenyi.janos96@gmail.com>
2023-02-23 19:23:16 +0100

    Equalizer véletlen számú színes blokkok kiiratása

--- src/csomag/Equalizer.java
+++ src/csomag/Equalizer.java
@@ -2,6 +2,20 @@ package csomag;
 
 public class Equalizer {
     public static void main(String[] args){
+        eq();
+        eq();
+        eq();
+        eq();
+        eq();
+    }
+
+    private static void eq() {
+        int vel = (int) (Math.random() * 5) + 3;
+        String szin = "\u001B[45m";
+        for (int i = 0; i < vel; i++) {
+            System.out.print(szin + " ");
+        }
+        System.out.println("");
 
     }
 }
